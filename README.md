e97ef089b2d5f0b7bb50388695b5050041192f80
stevenhao <steven@scaleapi.com>
2020-03-13 16:33:04 -0700

    fixeus

---
+++ README.md
@@ -0,0 +1,16 @@
+Weak Person Feed
+
+## Running
+Run two processes:
+1. ```python3 main.py```
+2. ```./sync.sh```
+
+## Requirements
+- Intended to run on osx or linux (including rapsberry pi)
+- Intended to work with any video capture device, including built-in webcams, external webcams, and capture enabled cameras
+- Requires python 3 with open cv 2 installed (`pip3 install cv2`)
+- Requires aws cli
+
+## Storage & permissions
+You need to configure the aws cli to use an aws account: https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html
+- Edit `sync.sh` to use your own profile / bucket
--- main.py
+++ main.py
@@ -9,21 +9,24 @@ capture_id = "test_capture"
 def secs():
     return time.time()
 
-start_time = secs()
-for i in range(100):
-    ret, frame = cap.read()
+start_time = int(secs()) + 1
+frame_num = 0
+frame_delay = 1.0 # seconds/frame
+while True:
     now = datetime.datetime.now()
-    delay = start_time + i - secs()
+    delay = start_time + frame_delay * frame_num - secs()
     if delay > 0:
         time.sleep(delay)
     now = datetime.datetime.now()
+    ret, frame = cap.read()
     root_dir = "out/%s" % capture_id
     cur_dir = root_dir + "/" + now.strftime("%m/%d/%H/%M");
     Path(cur_dir).mkdir(parents=True, exist_ok=True)
-    cur_file = now.strftime("%S.jpg")
+    cur_file = now.strftime("%S.%f.jpg")
     path = cur_dir + "/" + cur_file
     print(path)
     try:
         cv2.imwrite(path, frame)     # save frame as JPEG file
     except:
         print("error on", path)
+    frame_num += 1
