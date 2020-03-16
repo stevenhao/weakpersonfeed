3e9136999a633ea31befcc864e4ef5cc0a4996d1
stevenhao <steven@scaleapi.com>
2020-03-16 16:07:26 -0700

    10 hr cutoff

--- sync.sh
+++ sync.sh
@@ -2,6 +2,6 @@ echo Syncing every 10 seconds...
 while :; do
   echo Syncing... $(date)
   aws --profile sven s3 sync out s3://weakpersonfeed/
-  find out -type f -mtime +1 | xargs rm
+  find out -mmin +600 -delete
   sleep 10
 done
