d53c0ec23fb42e460516c8caa14f746422458a17
stevenhao <steven@scaleapi.com>
2020-03-16 15:30:42 -0700

    delete old files

--- sync.sh
+++ sync.sh
@@ -2,5 +2,6 @@ echo Syncing every 10 seconds...
 while :; do
   echo Syncing... $(date)
   aws --profile sven s3 sync out s3://weakpersonfeed/
+  find out -type f -mtime +1 | xargs rm
   sleep 10
 done
